682bb7ffc5243b701f9b77b281d9e43b3f2d7755
AntoineCollin <antoine.collin@student.ecp.fr>
2023-11-06 17:12:11 +0100

    Gitignore issue fixed

--- experiment_script/htap_hyperparam_scheme_3.sh
+++ experiment_script/htap_hyperparam_scheme_3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 #SBATCH --account=cell     # The account name for the job.
-#SBATCH --job-name=htap_2      # The job name.
+#SBATCH --job-name=htap_debug      # The job name.
 #SBATCH --gres=gpu:1           # Request 1 gpu (Up to 2 gpus per GPU node)
 #SBATCH --partition=gpu
 #SBATCH --time=35:00:00           # The time the job will take to run in D-HH:MM
@@ -14,4 +14,4 @@ singularity_path=$working_dir"/singularity_scPermut.sif"
 
 module load singularity
 
-singularity exec --nv --bind $working_dir:$singularity_working_dir $singularity_path python /home/acollin/dca_permuted_workflow/workflow/workflow_hp.py --dataset_name htap_final_by_batch --class_key celltype --batch_key donor --use_hvg 5000 --mode entire_condition --obs_key donor --keep_obs PAH_688194 PAH_691048 PAH_693770 PAH_698029 PRC_16 PRC_18 PRC_3 PRC_8 --training_scheme training_scheme_2 &> /home/acollin/dca_permuted_workflow/experiment_script/htap_hyperparam_scheme_2.log
+singularity exec --nv --bind $working_dir:$singularity_working_dir $singularity_path python /home/acollin/dca_permuted_workflow/workflow/workflow_hp_debug.py --dataset_name htap_final_by_batch --class_key celltype --batch_key donor --use_hvg 10000 --mode entire_condition --obs_key donor --keep_obs PAH_688194 PAH_691048 PAH_693770 PAH_698029 PRC_16 PRC_18 PRC_3 PRC_8 --training_scheme training_scheme_2 &> /home/acollin/dca_permuted_workflow/experiment_script/htap_hyperparam_scheme_2.log
