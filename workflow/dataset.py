Adding fixes for tf data leaks

workflow/dataset.py
@@ -142,11 +142,9 @@ def load_dataset(dataset_name,dataset_dir):
         return adata
 
 
-
 class Dataset:
     def __init__(self, 
-                dataset_dir,
-                dataset_name,
+                adata,
                 class_key,
                 batch_key,
                 filter_min_counts,
@@ -157,13 +155,13 @@ class Dataset:
                 n_perm,
                 semi_sup,
                 unlabeled_category):
-        self.dataset_name = dataset_name
-        self.adata = anndata.AnnData()
+        self.adata = adata
         self.adata_train_extended = anndata.AnnData()
         self.adata_train = anndata.AnnData()
         self.adata_val = anndata.AnnData()
         self.adata_test = anndata.AnnData()
         self.class_key = class_key
+        self.adata.obs[f'true_{self.class_key}'] = self.adata.obs[self.class_key] # Duplicate original annotation in true_class_key
         self.batch_key = batch_key
         self.filter_min_counts = filter_min_counts
         self.normalize_size_factors = normalize_size_factors
@@ -185,66 +183,10 @@ class Dataset:
         self.true_celltype=str()
         self.false_celltype=str()
         self.pct_false=float()
+                
+        # self.markers_path = self.dataset_dir + '/' + f'markers/markers_{dataset_name}.csv'
         
-        self.dataset_dir = dataset_dir
-        self.dataset_names = {'htap':'htap_annotated',
-                                'lca' : 'LCA_log1p',
-                                'discovair':'discovair_V6',
-                              'discovair_V7':'discovair_V7',
-                              'discovair_V7_filtered':'discovair_V7_filtered_raw', # Filtered version with doublets, made lighter to pass through the model
-                              'discovair_V7_filtered_no_D53':'discovair_V7_filtered_raw_no_D53',
-                                'ajrccm':'HCA_Barbry_Grch38_Raw',
-                                "ajrccm_by_batch":"ajrccm_by_batch",
-                                "disco_htap_ajrccm":'discovair_htap_ajrccm',
-                                "disco_htap": 'discovair_htap',
-                                "disco_ajrccm": 'discovair_ajrccm',
-                                "disco_ajrccm_downsampled":'discovair_ajrccm_downsampled',
-                                "discovair_ajrccm_small" : "discovair_ajrccm_small",
-                                'htap_ajrccm': 'htap_ajrccm_raw',
-                             'pbmc3k_processed':'pbmc_3k',
-                             'htap_final':'htap_final',
-                             'htap_final_by_batch':'htap_final_by_batch',
-                             'htap_final_C1_C5':'htap_final_C1_C5',
-                             'pbmc8k':'pbmc8k',
-                             'pbmc68k':'pbmc68k',
-                             'pbmc8k_68k':'pbmc8k_68k',
-                             'pbmc8k_68k_augmented':'pbmc8k_68k_augmented',
-                             'pbmc8k_68k_downsampled':'pbmc8k_68k_downsampled',
-                             'htap_final_ajrccm': 'htap_final_ajrccm',
-                             'hlca_par_sample_harmonized':'hlca_par_sample_harmonized',
-                             'hlca_par_dataset_harmonized':'hlca_par_dataset_harmonized',
-                             'hlca_trac_sample_harmonized':'hlca_trac_sample_harmonized',
-                             'hlca_trac_dataset_harmonized':'hlca_trac_dataset_harmonized'}
-        # self.batch_key = {'htap': 'place_holder',
-        #                     'discovair': 'sample',
-        #                     'ajrccm': 'manip',
-        #                     'pbmc8k':'dataset',
-        #                     'pbmc68k':'dataset',
-        #                     'pbmc8k_68k':'dataset',
-        #                     'pbmc8k_68k_augmented':'dataset',
-        #                     'pbmc8k_68k_downsampled':'dataset',
-        #                     'disco_htap_ajrccm': 'sample',
-        #                     'disco_htap': 'sample',
-        #                     'disco_ajrccm': 'manip',
-        #                     'disco_ajrccm_downsampled': 'manip',
-        #                     'htap_ajrccm': 'place_holder',
-        #                     'pbmc3k_processed': 'manip',
-        #                     'htap_final': 'sample',
-        #                     'htap_final_C1_C5':'sample',
-        #                     'discovair_ajrccm_small' : 'manip',
-        #                     'htap_final_ajrccm': 'place_holder'}
-        # self.batch_key = self.batch_key[self.dataset_name]
-        self.dataset_path = self.dataset_dir + '/' + self.dataset_names[self.dataset_name] + '.h5ad'
-        
-        self.markers_path = self.dataset_dir + '/' + f'markers/markers_{dataset_name}.csv'
-        
-    def load_dataset(self):
-        self.adata = sc.read_h5ad(self.dataset_path)
-        self.adata.obs[f'true_{self.class_key}'] = self.adata.obs[self.class_key] # Duplicate original annotation in true_class_key
-#         if type(self.adata.X) != np.ndarray :
-#             self.adata.X = self.adata.X.toarray()
-        if not self.adata.raw:
-            self.adata.raw = self.adata
+
         
         
     def normalize(self):

workflow/workflow_hp.py
@@ -2,7 +2,7 @@ import sys
 import os
 try :
     from .load import load_runfile
-    from .dataset import Dataset
+    from .dataset import Dataset, load_dataset
     from .predictor import MLP_Predictor
     from .model import DCA_Permuted,Scanvi,DCA_into_Perm, ScarchesScanvi_LCA
     from .utils import get_optimizer, scanpy_to_input, default_value, str2bool
@@ -10,7 +10,7 @@ try :
 
 except ImportError:
     from load import load_runfile
-    from dataset import Dataset
+    from dataset import Dataset, load_dataset
     from predictor import MLP_Predictor
     from model import DCA_Permuted,Scanvi
     from utils import get_optimizer, scanpy_to_input, default_value, str2bool
@@ -389,12 +389,14 @@ class Workflow:
         self.layer2 =  params['layer2']
         self.bottleneck = params['bottleneck']
 
-        self.ae_hidden_size = [self.layer1, self.layer2, self.bottleneck, self.layer1, self.layer2]
+        self.ae_hidden_size = [self.layer1, self.layer2, self.bottleneck, self.layer2, self.layer1]
 
         self.dann_hidden_dropout, self.class_hidden_dropout, self.ae_hidden_dropout = self.dropout, self.dropout, self.dropout
+
+        adata = load_dataset(dataset_dir = self.data_dir,
+                               dataset_name = self.dataset_name)
         
-        self.dataset = Dataset(dataset_dir = self.data_dir,
-                               dataset_name = self.dataset_name,
+        self.dataset = Dataset(adata = adata,
                                class_key = self.class_key,
                                batch_key= self.batch_key,
                                filter_min_counts = self.filter_min_counts,
@@ -405,7 +407,6 @@ class Workflow:
                                n_perm = self.n_perm,
                                semi_sup = self.semi_sup,
                                unlabeled_category = self.unlabeled_category)
-        self.dataset.load_dataset()
         self.dataset.normalize()
         self.dataset.train_split(mode = self.mode,
                             pct_split = self.pct_split,
@@ -526,12 +527,13 @@ class Workflow:
                     np.save(save_dir + f'umap_{group}.npy', pred_adata.obsm['X_umap'])
                     self.run[f'evaluation/{group}/umap'].track_files(save_dir + f'umap_{group}.npy')
                     sc.set_figure_params(figsize=(15, 10), dpi = 300)
-                    fig_class = sc.pl.umap(pred_adata, color = self.class_key, size = 5,return_fig = True)
+                    fig_class = sc.pl.umap(pred_adata, color = f'true_{self.class_key}', size = 5,return_fig = True)
                     fig_batch = sc.pl.umap(pred_adata, color = self.batch_key, size = 5,return_fig = True)
                     fig_split = sc.pl.umap(pred_adata, color = 'train_split', size = 5,return_fig = True)
                     self.run[f'evaluation/{group}/classif_umap'].upload(fig_class)
                     self.run[f'evaluation/{group}/batch_umap'].upload(fig_batch)
                     self.run[f'evaluation/{group}/split_umap'].upload(fig_split)
+
         inp = scanpy_to_input(adata_list['val'],['size_factors'])
         inp = {k:tf.convert_to_tensor(v) for k,v in inp.items()}
         _, clas, dann, rec = self.dann_ae(inp, training=False).values()
@@ -539,8 +541,19 @@ class Workflow:
         opt_metric = self.metrics_list['mcc'](np.asarray(y_list['val'].argmax(axis=1)), clas.argmax(axis=1)) # We retrieve the last metric of interest
         if self.log_neptune:
             self.run.stop()
+        del self.dann_ae
+        del self.dataset
+        del history
+        del self.optimizer
+        del self.rec_loss_fn
+        del self.clas_loss_fn
+        del self.dann_loss_fn
+        del self.metric_list
+
         gc.collect()
         tf.keras.backend.clear_session()
+        tf.clear_default_graph()
+
         return opt_metric
 
     def train_scheme(self,
@@ -669,6 +682,7 @@ class Workflow:
         elif training_strategy == "permutation_only":
             group = 'train'
             self.freeze_block(ae, 'all_but_autoencoder')
+            use_perm = True
 
         if not use_perm:
             use_perm = True
@@ -719,10 +733,11 @@ class Workflow:
             gradients = tape.gradient(loss, ae.trainable_variables)
             optimizer.apply_gradients(zip(gradients, ae.trainable_variables))
 
-            mean_loss = self.mean_loss_fn(loss)
-            mean_clas_loss = self.mean_clas_loss_fn(clas_loss)
-            mean_dann_loss = self.mean_dann_loss_fn(dann_loss)
-            mean_rec_loss = self.mean_rec_loss_fn(rec_loss)
+            self.mean_loss_fn(loss)
+            self.mean_clas_loss_fn(clas_loss)
+            self.mean_dann_loss_fn(dann_loss)
+            self.mean_rec_loss_fn(rec_loss)
+
             if verbose :
                 self.print_status_bar(n_samples, n_obs, [self.mean_loss_fn, self.mean_clas_loss_fn, self.mean_dann_loss_fn, self.mean_rec_loss_fn], self.metrics)
         self.print_status_bar(n_samples, n_obs, [self.mean_loss_fn, self.mean_clas_loss_fn, self.mean_dann_loss_fn, self.mean_rec_loss_fn], self.metrics)
@@ -1038,6 +1053,7 @@ if __name__ == '__main__':
     run_file = parser.parse_args()
     working_dir = '/home/acollin/dca_permuted_workflow/'
     workflow = Workflow(run_file=run_file, working_dir=working_dir)
+    print("Workflow loaded")
 
     hparams = [
         #{"name": "use_hvg", "type": "range", "bounds": [5000, 10000], "log_scale": False},

workflow/workflow_run.py
@@ -187,7 +187,7 @@ class Workflow:
         # self.optimizer = self.run_file.optimizer
         # self.verbose = self.run_file[model_training_spec][verbose] # TODO : not implemented yet for DANN_AE
         # self.threads = self.run_file[model_training_spec][threads] # TODO : not implemented yet for DANN_AE
-        self.learning_rate = self.run_file.learning_rate
+        self.learning_rate = self.run_file.lr
         self.n_perm = 1
         self.semi_sup = False # TODO : Not yet handled by DANN_AE, the case wwhere unlabeled cells are reconstructed as themselves
         self.unlabeled_category = 'UNK' # TODO : Not yet handled by DANN_AE, the case wwhere unlabeled cells are reconstructed as themselves
@@ -271,7 +271,7 @@ class Workflow:
 
         ##### TODO : Add to runfile
 
-
+        self.warmup_epoch = self.run_file.warmup_epoch
 
         self.clas_loss_name = self.run_file.clas_loss_name
         self.clas_loss_name = default_value(self.clas_loss_name, 'MSE')
@@ -285,7 +285,7 @@ class Workflow:
         self.rec_loss_fn = None
 
         # self.weight_decay = self.run_file.weight_decay
-        self.weight_decay = None
+        self.weight_decay = self.run_file.wd
         self.optimizer_type = self.run_file.optimizer_type
         self.optimizer_type = default_value(self.optimizer_type , 'adam')
 
@@ -303,7 +303,7 @@ class Workflow:
         self.ae_activation = self.run_file.ae_activation
         self.ae_activation = default_value(self.ae_activation , "relu")
         self.ae_output_activation = self.run_file.ae_output_activation
-        self.ae_output_activation = default_value(self.ae_output_activation , "linear")
+        self.ae_output_activation = default_value(self.ae_output_activation , "relu")
         self.ae_init = self.run_file.ae_init
         self.ae_init = default_value(self.ae_init , 'glorot_uniform')
         self.ae_batchnorm = self.run_file.ae_batchnorm
@@ -376,23 +376,8 @@ class Workflow:
     def check_umap_log(self):
         return os.path.isfile(self.umap_log_path)
 
-    def make_experiment(self, params):
-        print(params)
+    def make_experiment(self):
 #        self.use_hvg = params['use_hvg']
-        self.clas_w =  params['clas_w']
-        self.dann_w = params['dann_w']
-        self.rec_w =  1
-        self.weight_decay =  params['wd']
-        self.warmup_epoch =  params['warmup_epoch']
-        self.dropout =  params['dropout']
-        self.layer1 = params['layer1']
-        self.layer2 =  params['layer2']
-        self.bottleneck = params['bottleneck']
-
-        self.ae_hidden_size = [self.layer1, self.layer2, self.bottleneck, self.layer1, self.layer2]
-
-        self.dann_hidden_dropout, self.class_hidden_dropout, self.ae_hidden_dropout = self.dropout, self.dropout, self.dropout
-
         adata = load_dataset(dataset_dir = self.data_dir,
                                dataset_name = self.dataset_name)
         
@@ -461,9 +446,9 @@ class Workflow:
             for par,val in self.run_file.__dict__.items():
                 self.run[f"parameters/{par}"] = stringify_unsupported(val)
 
-            for par,val in params.items():
-                self.run[f"parameters/{par}"] = stringify_unsupported(val)
-            self.run[f'parameters/ae_hidden_size'] = stringify_unsupported(self.ae_hidden_size)
+            # for par,val in params.items():
+            #     self.run[f"parameters/{par}"] = stringify_unsupported(val)
+            # self.run[f'parameters/ae_hidden_size'] = stringify_unsupported(self.ae_hidden_size)
 
         self.dann_ae = DANN_AE(ae_hidden_size=self.ae_hidden_size,
                         ae_hidden_dropout=self.ae_hidden_dropout,
@@ -544,10 +529,14 @@ class Workflow:
         del self.dann_ae
         del self.dataset
         del history
+        del self.optimizer
+        del self.rec_loss_fn
+        del self.clas_loss_fn
+        del self.dann_loss_fn
         gc.collect()
         tf.keras.backend.clear_session()
         
-        
+
         return opt_metric
 
     def train_scheme(self,
@@ -587,7 +576,7 @@ class Workflow:
                 wait = 0
                 best_epoch = 0
                 es_best = np.inf # initialize early_stopping
-                patience = 0
+                patience = 20
                 if strategy == 'permutation_only':
                     monitored = 'rec_loss'
                 else:
@@ -731,6 +720,7 @@ class Workflow:
             self.mean_clas_loss_fn(clas_loss)
             self.mean_dann_loss_fn(dann_loss)
             self.mean_rec_loss_fn(rec_loss)
+
             if verbose :
                 self.print_status_bar(n_samples, n_obs, [self.mean_loss_fn, self.mean_clas_loss_fn, self.mean_dann_loss_fn, self.mean_rec_loss_fn], self.metrics)
         self.print_status_bar(n_samples, n_obs, [self.mean_loss_fn, self.mean_clas_loss_fn, self.mean_dann_loss_fn, self.mean_rec_loss_fn], self.metrics)
@@ -993,7 +983,8 @@ if __name__ == '__main__':
     parser.add_argument('--normalize_size_factors', type=str2bool, nargs='?',const=True, default=True, help ='Weither to normalize dataset or not')
     parser.add_argument('--scale_input', type=str2bool, nargs='?',const=False, default=False, help ='Weither to scale input the count values')
     parser.add_argument('--logtrans_input', type=str2bool, nargs='?',const=True, default=True, help ='Weither to log transform count values')
-    parser.add_argument('--use_hvg', type=int, nargs='?', const=10000, default=None, help = "Number of hvg to use. If no tag, don't use hvg.")
+    parser.add_argument('--use_hvg', type=int, nargs='?', const=5000, default=None, help = "Number of hvg to use. If no tag, don't use hvg.")
+
     # parser.add_argument('--reduce_lr', type = , default = , help ='')
     # parser.add_argument('--early_stop', type = , default = , help ='')
     parser.add_argument('--batch_size', type = int, nargs='?', default = 256, help ='Training batch size')
@@ -1011,19 +1002,21 @@ if __name__ == '__main__':
     parser.add_argument('--true_celltype', type = str,nargs='?', default = None, help ='')
     parser.add_argument('--false_celltype', type = str,nargs='?', default = None, help ='')
     parser.add_argument('--pct_false', type = float,nargs='?', default = None, help ='')
+
+    parser.add_argument('--warmup_epoch', type=int, nargs='?', default=50, help ='Number of dann warmup epochs')
     parser.add_argument('--clas_loss_name', type = str,nargs='?', choices = ['categorical_crossentropy'], default = 'categorical_crossentropy' , help ='Loss of the classification branch')
     parser.add_argument('--dann_loss_name', type = str,nargs='?', choices = ['categorical_crossentropy'], default ='categorical_crossentropy', help ='Loss of the DANN branch')
     parser.add_argument('--rec_loss_name', type = str,nargs='?', choices = ['MSE'], default ='MSE', help ='Reconstruction loss of the autoencoder')
-    # parser.add_argument('--weight_decay', type = float,nargs='?', default = 1e-4, help ='Weight decay applied by th optimizer')
-    parser.add_argument('--learning_rate', type = float,nargs='?', default = 0.001, help ='Starting learning rate for training')
+    parser.add_argument('--wd', type = float,nargs='?', default = 1e-4, help ='Weight decay applied by the optimizer')
+    parser.add_argument('--lr', type = float,nargs='?', default = 0.001, help ='Starting learning rate for training')
     parser.add_argument('--optimizer_type', type = str, nargs='?',choices = ['adam','adamw','rmsprop'], default = 'adam' , help ='Name of the optimizer to use')
     parser.add_argument('--clas_w', type = float,nargs='?', default = 0.1, help ='Wight of the classification loss')
     parser.add_argument('--dann_w', type = float,nargs='?', default = 0.1, help ='Wight of the DANN loss')
     parser.add_argument('--rec_w', type = float,nargs='?', default = 0.8, help ='Wight of the reconstruction loss')
-    parser.add_argument('--ae_hidden_size', type = int,nargs='+', default = [128,64,128], help ='Hidden sizes of the successive ae layers')
+    parser.add_argument('--ae_hidden_size', type = int,nargs='+', default = [512,256,64,256,512], help ='Hidden sizes of the successive ae layers')
     parser.add_argument('--ae_hidden_dropout', type =float, nargs='?', default = None, help ='')
     parser.add_argument('--ae_activation', type = str ,nargs='?', default = 'relu' , help ='')
-    parser.add_argument('--ae_output_activation', type = str,nargs='?', default = 'linear', help ='')
+    parser.add_argument('--ae_output_activation', type = str,nargs='?', default = 'relu', help ='')
     parser.add_argument('--ae_init', type = str,nargs='?', default = 'glorot_uniform', help ='')
     parser.add_argument('--ae_batchnorm', type=str2bool, nargs='?',const=True, default=True , help ='')
     parser.add_argument('--ae_l1_enc_coef', type = float,nargs='?', default = None, help ='')
@@ -1041,38 +1034,9 @@ if __name__ == '__main__':
     parser.add_argument('--training_scheme', type = str,nargs='?', default = 'training_scheme_1', help ='')
     parser.add_argument('--log_neptune', type=str2bool, nargs='?',const=True, default=True , help ='')
     parser.add_argument('--workflow_id', type=str, nargs='?', default='default', help ='')
-    # parser.add_argument('--epochs', type=int, nargs='?', default=100, help ='')
 
     run_file = parser.parse_args()
     working_dir = '/home/acollin/dca_permuted_workflow/'
     workflow = Workflow(run_file=run_file, working_dir=working_dir)
     print("Workflow loaded")
-
-    hparams = [
-        #{"name": "use_hvg", "type": "range", "bounds": [5000, 10000], "log_scale": False},
-        {"name": "clas_w", "type": "range", "bounds": [1e-4, 1e2], "log_scale": False},
-        {"name": "dann_w", "type": "range", "bounds": [1e-4, 1e2], "log_scale": False},
-        {"name": "lr", "type": "range", "bounds": [1e-4, 1e-2], "log_scale": True},
-        {"name": "wd", "type": "range", "bounds": [1e-8, 1e-4], "log_scale": True},
-        {"name": "warmup_epoch", "type": "range", "bounds": [1, 3]},
-        {"name": "dropout", "type": "range", "bounds": [0.0, 0.5]},
-        {"name": "bottleneck", "type": "range", "bounds": [32, 64]},
-        {"name": "layer2", "type": "range", "bounds": [64, 512]},
-        {"name": "layer1", "type": "range", "bounds": [512, 1024]},
-
-    ]
-
-    # workflow.make_experiment(hparams)
-
-    best_parameters, values, experiment, model = optimize(
-        parameters=hparams,
-        evaluation_function=workflow.make_experiment,
-        objective_name='mcc',
-        minimize=False,
-        total_trials=30,
-        random_seed=40,
-
-    )
-
-print(best_parameters)
-
+    workflow.make_experiment()