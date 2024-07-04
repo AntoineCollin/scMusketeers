3660a80ecf94a5b2e57380d8526df2517883c09a
Christophe Bécavin <becavin.christophe@gmail.com>
2024-07-04 15:56:17 +0200

    Add tutoriel.py to create tutorial data, and update tutorial.sh with use-cases

--- tutorial.sh
+++ tutorial.sh
@@ -6,7 +6,7 @@ outdir=/data/analysis/data_becavin/scpermut_test/tutorial
 python scpermut/__main__.py transfer $dataset --class_key=celltype --unlabeled_category="Unknown" --batch_key=manip --out=$outdir
 
 # Transfer Cell annotation and remove batch to query adata
-python scpermut/__main__.py transfer ${ref_dataset} --query_path ${query_dataset} --class_key=celltype --unlabeled_category="Unknown" --batch_key=manip --out=$outdir
+#python scpermut/__main__.py transfer ${ref_dataset} --query_path ${query_dataset} --class_key=celltype --unlabeled_category="Unknown" --batch_key=manip --out=$outdir
 
 ##### Sampling_percentage 40%
 # Transfer Cell annotation to all Unknown cells
