e527c5acd0e17c08a1eb0f7ddf73e41233125bcf
Christophe Bécavin <becavin.christophe@gmail.com>
2024-10-21 20:48:12 +0200

    Update tutorial with CellTypist Lung

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -42,16 +42,18 @@ def split_adata(adata, cats, train_size=0.8):
 
 
 def create_tuto_data(
+    subsample,
     sampling_percentage,
     path_adata,
+    working_dir,
     name,
     class_key,
     batch_key,
     unlabeled_category,
 ):
-    adata = sc.read_h5ad(path_adata)
+    adata = sc.read_h5ad(os.path.join(working_dir,path_adata))
     adata_train, adata_test = split_adata(
-        adata, batch_key, 1 - sampling_percentage
+        adata, batch_key, 1 - subsample
     )
 
     # Compute UMAP
@@ -60,7 +62,7 @@ def create_tuto_data(
         adata_test, n_neighbors=10, n_pcs=40
     )  # UMAP is based on the neighbor graph; we'll compute this first
     sc.tl.umap(adata_test)
-    sc.pl.umap(adata_test, color="celltype")
+    sc.pl.umap(adata_test, color=class_key)
 
     # add unlabeled_category to celltype
     celltype = adata_test.obs[class_key]
@@ -80,32 +82,27 @@ def create_tuto_data(
     print(celltype)
     adata_test.obs[class_key] = celltype
     path_new_adata = os.path.join(
-        "data", f"{name}-unknown-{sampling_percentage}.h5ad"
+        working_dir,"data", f"{name}-unknown-{sampling_percentage}.h5ad"
     )
     adata_test.write_h5ad(path_new_adata)
 
     # unassigned X% of cells, keeping cell type distribution (Useful ???)
 
     #### Batch correction
+    # sample adata to reduce its size
     adata_train, adata_test = split_adata(
-        adata, batch_key, 1 - sampling_percentage
+        adata, batch_key, 1 - subsample
     )
-    # separate cells from 2X% of batches
-    print(np.arange(adata.n_obs))
     # sample adata to reduce its size
     all_batches = adata_test.obs[batch_key].unique()
     print(len(all_batches))
     test_batches = np.random.choice(
         all_batches, int(len(all_batches) * sampling_percentage), replace=False
     )
-    print(test_batches)
-    print(len(test_batches))
     train_batches = list(set(all_batches).difference(test_batches))
     adata_ref = adata_test[adata_test.obs[batch_key].isin(train_batches)]
     adata_query = adata_test[adata_test.obs[batch_key].isin(test_batches)]
-    print(adata_ref.obs[batch_key])
-    print(adata_query.obs[batch_key])
-
+    
     # Compute PCA and UMAP
     sc.tl.pca(adata_ref, svd_solver="arpack")
     sc.pp.neighbors(
@@ -127,11 +124,11 @@ def create_tuto_data(
 
     ### Ref and query are separated
     path_new_adata = os.path.join(
-        "data", f"{name}-ref-batch-{sampling_percentage}.h5ad"
+        working_dir,"data", f"{name}-ref-batch-{sampling_percentage}.h5ad"
     )
     adata_ref.write_h5ad(path_new_adata)
     path_new_adata = os.path.join(
-        "data", f"{name}-query-batch-{sampling_percentage}.h5ad"
+        working_dir,"data", f"{name}-query-batch-{sampling_percentage}.h5ad"
     )
     adata_query.write_h5ad(path_new_adata)
 
